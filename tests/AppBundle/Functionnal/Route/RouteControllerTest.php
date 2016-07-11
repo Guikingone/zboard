98fee3eff6847edb3eb36508289f0c0cf08af517
Guikingone <guillaume.loulier@hotmail.fr>
2016-07-11 08:29:00 +0200

    [+][!M] || Dependencies files added + Routes test added

---
+++ tests/AppBundle/Functionnal/Route/RouteControllerTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Tests\AppBundle\Functionnal\Route;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class RouteControllerTest extends WebTestCase
+{
+    /**
+     * @dataProvider urlProvider
+     */
+    public function testPageIsSuccessful($url)
+    {
+        $client = self::createClient();
+        $client->request('GET', $url);
+
+        $this->assertTrue($client->getResponse()->isSuccessful());
+    }
+
+    /**
+     * @return array
+     *
+     * Provide the different routes through the application
+     */
+    public function urlProvider()
+    {
+        return array(
+            array('/')
+        );
+    }
+}
