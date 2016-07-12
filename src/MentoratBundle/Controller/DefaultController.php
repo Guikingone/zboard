047811305d0e684194a00b9fbe7d2fca73c8cf6d
Etoile Filante <nathanael.langlois@gmail.com>
2016-07-12 11:26:05 -0400

    Added Index view for MentorBundle

--- src/MentoratBundle/Controller/DefaultController.php
+++ src/MentoratBundle/Controller/DefaultController.php
@@ -12,6 +12,6 @@ class DefaultController extends Controller
      */
     public function indexAction()
     {
-        return $this->render('MentorBundle:Default:index.html.twig');
+        return $this->render('MentorBundle:Index:index.html.twig');
     }
 }
