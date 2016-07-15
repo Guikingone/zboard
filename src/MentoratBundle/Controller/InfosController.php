d9318d6fd7a8ebd7b24897fcb925c702b3027e90
Etoile Filante <nathanael.langlois@gmail.com>
2016-07-15 10:07:56 -0400

    Added info page controller

---
+++ src/MentoratBundle/Controller/InfosController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace MentoratBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+class InfosController extends Controller
+{
+  /**
+   * @Route("/infos")
+   * @Template("MentoratBundle/Dashboard/infos.html.twig")
+   */
+  public function indexAction()
+  {
+      return array(
+          "controller" => "infos"
+      );
+  }
+}
