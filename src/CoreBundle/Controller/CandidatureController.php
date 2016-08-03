f8200d5ad34746e15efafd6605b8d4eb2d54a842
Etoile Filante <nathanael.langlois@gmail.com>
2016-08-03 12:18:31 -0400

    moved candidate entity

--- src/CoreBundle/Controller/CandidatureController.php
+++ src/CoreBundle/Controller/CandidatureController.php
@@ -4,14 +4,17 @@ namespace CoreBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
 class CandidatureController extends Controller
 {
     /**
    * @Route("/join",name="join")
+   * @Template("CoreBundle/Default/candidature.html.twig")
    */
   public function indexAction()
   {
-      return $this->render('CoreBundle/Default/candidature.html.twig');
-  }
+    return array(
+        'controller' => 'candidature',
+    );  }
 }
