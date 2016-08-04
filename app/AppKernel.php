a5f45ab3a31c1cee8e9a060a207301447b202050
Christophe Lablancherie <c.lablancherie@live.fr>
2016-08-04 14:07:31 +0200

    Rajout doctrines fixtures dans le kernel

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -22,6 +22,7 @@ class AppKernel extends Kernel
             new BackendBundle\BackendBundle(),
             new CoreBundle\CoreBundle(),
             new NotificationBundle\NotificationBundle(),
+            new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
