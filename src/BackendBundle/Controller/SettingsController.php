b1dccfa6739014d326e378d3360923cb720c495f
Etoile Filante <nathanael.langlois@gmail.com>
2016-08-06 21:55:48 -0400

    created settings page

---
+++ src/BackendBundle/Controller/SettingsController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace BackendBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Request;
+
+class SettingsController extends Controller
+{
+  /**
+   * @param Request $request
+   *
+   * @return array
+   *
+   * @Route("/settings", name="settings")
+   * @Template("BackBundle/Index/settings.html.twig")
+   */
+  public function showMentorsAction(Request $request)
+  {
+      return array(
+          'controller' => 'settings',
+      );
+  }
+}
