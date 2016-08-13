fd8534fe1d81bf261b0ea5d02cfce9da72ca3b81
Guikingone <guillaume.loulier@hotmail.fr>
2016-08-13 11:38:51 +0200

    [+][!M] || Corrections vues|En cours

--- src/AdminBundle/Services/Admin.php
+++ src/AdminBundle/Services/Admin.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the Zboard project.
+ *
+ * (c) Guillaume Loulier <guillaume.loulier@hotmail.fr>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace AdminBundle\Services;
 
 use AdminBundle\Entity\Country;
