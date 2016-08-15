c3395faf1e954086940a31afa72cb10b1354586b
Nathanaël Langlois <nathanael.langlois@gmail.Com>
2016-08-15 11:25:25 +0200

    Added vote type

---
+++ src/MentoratBundle/Form/VoteType.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace MentoratBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class VoteType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array                $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('message', TextareaType::class)
+            ->add('enabled', ChoiceType::class, array(
+                'vote' => [
+                    'Pour (+1)' => 'true',
+                    'Contre (-1)' => 'false',
+                ],
+            ))
+        ;
+    }
+
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'MentoratBundle\Entity\RecrutementVote',
+        ));
+    }
+}
