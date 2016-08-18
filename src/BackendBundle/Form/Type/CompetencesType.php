d3dd36df36bd8da3ff3e063d6fd5712de907bad7
Christophe Lablancherie <c.lablancherie@live.fr>
2016-08-18 09:54:22 +0200

    Rajout des formTypes

---
+++ src/BackendBundle/Form/Type/CompetencesType.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace BackendBundle\Form\Type;
+
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CompetencesType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array                $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('libelle', EntityType::class, array(
+                'class' => 'BackendBundle\Entity\Competences',
+                'choice_label' => 'libelle',
+            ))
+        ;
+    }
+
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'BackendBundle\Entity\Competences',
+        ));
+    }
+}
