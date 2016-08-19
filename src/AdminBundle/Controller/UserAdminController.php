8bc6479805c7cb8b1553ed0cb5053fdb65ef296c
Guikingone <guillaume.loulier@hotmail.fr>
2016-08-19 08:45:44 +0200

    [+][!M] || Corrections vues|En cours

--- src/AdminBundle/Controller/UserAdminController.php
+++ src/AdminBundle/Controller/UserAdminController.php
@@ -6,7 +6,6 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Request;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 
 /**
  * @Route("/users")
